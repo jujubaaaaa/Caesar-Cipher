c414b2f5e442e332c074f1f9dd520a35155d162d
jujubaaaaa <julia.pereira.am84@gmail.com>
2024-05-16 12:26:16 -0300

    aula de hpje 16 do 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, flash
 
 def create_app():
     
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hfdsjkeshfdueirdh'
 
-    @app.route("/")
+    @app.route('/')
     def home(name='Home'):
         return render_template('index.html', name=name)
     
-    @app.route("/encode")
+    @app.route('/encode', methods=('GET', 'POST'))
     def encode(name='Encode'):
+        if request.method == 'POST':
+           cipher_key = request.form['cipher_key']
+           message = request.form['message']
+
+           if not cipher_key:
+              flash('A chave é obrigatória...')
+           elif not message:  
+              flash('A mensagem é necessária...') 
+           else:
+              flash("ENCODE")   
+
+
+
         return render_template('encode.html', name=name)
 
 
