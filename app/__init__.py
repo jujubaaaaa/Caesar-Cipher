9ac0cd1af0efdbec8d5605a85e4a631085b9b396
jujubaaaaa <julia.pereira.am84@gmail.com>
2024-05-21 11:57:37 -0300

    aula de hoje 21/05

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash
+from app.encode import encode_message
 
 def create_app():
     
@@ -7,24 +8,28 @@ def create_app():
 
     @app.route('/')
     def home(name='Home'):
-        return render_template('index.html', name=name)
+      return render_template('index.html', name=name)
     
     @app.route('/encode', methods=('GET', 'POST'))
     def encode(name='Encode'):
-        if request.method == 'POST':
-           cipher_key = request.form['cipher_key']
-           message = request.form['message']
+      if request.method == 'POST':
+         try: 
+            cipher_key =int(request.form['cipher_key']) 
+         except:
+            cipher_key = 0   
 
-           if not cipher_key:
-              flash('A chave é obrigatória...')
-           elif not message:  
-              flash('A mensagem é necessária...') 
-           else:
-              flash("ENCODE")   
+         message = request.form['message']
+
+         if not cipher_key:
+            flash('A chave é obrigatória...')
+         elif not message:  
+            flash('A mensagem é necessária...') 
+         else:
+            flash("ENCODE")   
+           
 
 
-
-        return render_template('encode.html', name=name)
+      return render_template('encode.html', name=name)
 
 
     return app 
